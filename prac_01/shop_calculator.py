3056dc90fac9bef48123f0bee44125ebb8b8f318
CalCobalt <Chara_2027@qq.com>
2025-09-04 09:02:18 +0800

    modified:   prac_01/shop_calculator.py

--- prac_01/shop_calculator.py
+++ prac_01/shop_calculator.py
@@ -1,6 +1,11 @@
-
-
-count = 1
-price, count = input("Enter price and count: ").split()
-price = float(price)
-count = int(count)
+kinds = int(input("Enter number of kinds of items: "))
+total = 0.00
+for i in range(1, kinds + 1):
+    price, count = input("Enter price and count: ").split()
+    price = float(price)
+    count = int(count)
+    total += price * count
+if total >= 100:
+    total = total * 0.9
+    print("10% discount applied!")
+print("Total price is:", '%.2f'%total)
