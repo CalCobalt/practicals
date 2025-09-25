d4d64d513d4232829b0ba07e6b435bd3983a5116
CalCobalt <Chara_2027@qq.com>
2025-09-25 20:30:58 +0800

    4

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,23 @@
+import random
+
+NUMBERS_PER_PICK = 6
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+
+def main():
+    number_of_picks = int(input("How many quick picks? "))
+    for _ in range(number_of_picks):
+        quick_pick = generate_quick_pick()
+        print(" ".join(f"{num:2}" for num in quick_pick))
+
+
+def generate_quick_pick():
+    numbers = []
+    while len(numbers) < NUMBERS_PER_PICK:
+        number = random.randint(MIN_NUMBER, MAX_NUMBER)
+        if number not in numbers:  # 确保不重复
+            numbers.append(number)
+    numbers.sort()
+    return numbers
+
+main()
